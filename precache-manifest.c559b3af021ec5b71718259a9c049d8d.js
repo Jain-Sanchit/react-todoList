25d7f6e261483c147993ffc219daabf18e622561
Jain-Sanchit <Jainsanchit157@gmail.com>
2020-06-06 21:39:09 +0530

    Updates

---
+++ precache-manifest.c559b3af021ec5b71718259a9c049d8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771076b200388b1987e66f43a79fb58a",
+    "url": "/react-todoList/index.html"
+  },
+  {
+    "revision": "3405a9cad6d201b7308e",
+    "url": "/react-todoList/static/css/main.9bb40c2d.chunk.css"
+  },
+  {
+    "revision": "3109378068876897d3e2",
+    "url": "/react-todoList/static/js/2.4609f815.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todoList/static/js/2.4609f815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3405a9cad6d201b7308e",
+    "url": "/react-todoList/static/js/main.b6bbe952.chunk.js"
+  },
+  {
+    "revision": "3d5cff83bd17c7ec2858",
+    "url": "/react-todoList/static/js/runtime-main.ca949a7c.js"
+  }
+]);
